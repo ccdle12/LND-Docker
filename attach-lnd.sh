49c1450e85604305d11d3bc83d7239ac78b7447b
ccdle12 <chris.coverdale24@gmail.com>
2018-10-10 10:27:04 +0100

    lnd.sh: adding control script
    This script will contain all the commands to interact with the LND
    Container. For example, `./lnd.sh run` will run the container and
    `./lnd.sh attach` will attach into the container. This gives the user a
    single point to interact with the container from the host machine.

--- attach-lnd.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker exec -ti lnd-node /bin/bash
--- run-docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Build the current Dockerfile as tagged image.
-docker build -t "lnd" ./
-
-# $1 = Path to host persistent storage.
-
-# Run the container.
-docker run -d --env-file=.env -p 10009:10009 -p 8080:8080 -p 8334:8334 -p 8337:8337 -v $1:/mnt/btcd --name lnd-node lnd
