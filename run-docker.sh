d5724a0d36f2675a73c85079fc6e17dcb104a06b
ccdle12 <chris.coverdale24@gmail.com>
2018-10-10 10:07:57 +0100

    run-docker.sh: adding optional arg for volume mnt path

--- run-docker.sh
+++ run-docker.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# Build the current Dockerfile as tagged image
+# Build the current Dockerfile as tagged image.
 docker build -t "lnd" ./
 
-# Runt the container
-docker run -d --env-file=.env -p 10009:10009 -p 8080:8080 -p 8334:8334 -p 8337:8337 --name lnd-node lnd
+# $1 = Path to host persistent storage.
+
+# Run the container.
+docker run -d --env-file=.env -p 10009:10009 -p 8080:8080 -p 8334:8334 -p 8337:8337 -v $1:/mnt/btcd --name lnd-node lnd
